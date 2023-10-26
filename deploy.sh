60720feb1c7bb71b92812eaa3fe486421b6cf490
suisuyy <suisuyus@gmail.com>
2023-10-26 04:19:39 +0000

    add vv

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,22 @@
 #!/bin/bash
 
 # readme
-#default user:passwd root:tt  gpt:tt t:tt test:tt , alist at port 4001 ttyd at port 4002,http /tmp at 4003 and 4010
 alias pacman='pacman --noconfirm'
 
+
 #utils
-pacman -Syu  --noconfirm wget curl ttyd aria2 w3m busybox
+pacman -Syu  --noconfirm wget curl ttyd aria2 w3m busybox git
 #python
 pacman -Syu  --noconfirm  python3 python-pip
-#node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install 18
-nvm use 18
 
-
-useradd -m -s /bin/bash t
-useradd -m -s /bin/bash gpt
-useradd -m -s /bin/bash test
-echo -e 'tt\ntt\n' | passwd root
-echo -e 'tt\ntt\n' | passwd t
-echo -e 'tt\ntt\n' | passwd gpt
-echo -e 'tt\ntt\n' | passwd test
-
-
-/bin/ttyd -p 4002  /bin/busybox login &
-cd /tmp
-python -m http.server --bind :: 4003 &
-#python -m http.server --bind :: 4010 &
-/bin/ttyd -p 4010  /bin/bash &
-/bin/ttyd -p 80  /bin/bash &
-#alist
+mkdir /root
 cd /root
-wget ${DAVURL}/alisthome.tar
-tar xf alisthome.tar
-cd alisthome/
-mkdir /sdata
-./alist server
+ git clone https://github.com/suisuyy/sbox.git
+ cd sbox/app/vv/
+bash run.sh
 
-echo 'end deploay.sh'
-python -m http.server --bind :: 40001 
-/bin/read
-/bin/bash
+
+
+
+
 echo 'end last'
